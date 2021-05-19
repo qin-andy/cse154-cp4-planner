3c4be626d61edc5dcf402984fa728af1834dedb7
Andy <andyqin17@gmail.com>
2021-05-19 16:43:27 -0700

    Add public folder

--- app.js
+++ app.js
@@ -3,7 +3,14 @@
 const express = require('express');
 const multer = require('multer');
 const app = express();
+
+
 app.use(multer().none());
+
+/* Include middlewares for handling POST requests */
+app.use(express.urlencoded({ extended: true })) // for application/x-www-form-urlencoded
+app.use(express.json()); // for application/json
+app.use(multer().none()); // for multipart/form-data (required with FormData)
 
 app.get('/hello', (request, response) => {
   console.log("Accessed hello");
