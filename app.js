1316e49bb114c6772673d90aedecead7f18dbaae
Andy <andyqin17@gmail.com>
2021-05-19 17:12:56 -0700

    Remove task links

--- app.js
+++ app.js
@@ -2,8 +2,10 @@
 
 const express = require('express');
 const multer = require('multer');
+const fs = require('fs').promises;
 const app = express();
 
+const tasks = [];
 
 app.use(multer().none());
 
